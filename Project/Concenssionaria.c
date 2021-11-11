01e27ee3d9307994ed695e8f84240ae7551cd1ff
Lodr Tsuki <xyzgames@outlook.com.br>
2021-11-10 21:19:20 -0300

    Concessionaria

---
+++ Project/Concenssionaria.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+printf("=======MENU=======");
+printf("=1 - Inserir     =");
+printf("=2 - Verificar   =");
+printf("=3 - Apagar      =");
+printf("=4 - Sair        =");
+printf("==================");
+switch (menu)
+{
+case 1:
+    /* code */
+    break;
+
+case 2:
+
+    break;
+
+case 3:
+
+    break;
+
+case 4:
+    exit(1)
+    break;
+default:
+    break;
+}
